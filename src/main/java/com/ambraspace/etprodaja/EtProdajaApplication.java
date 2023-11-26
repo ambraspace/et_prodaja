2de0faa6e7eedbbc114c08ff7ff491905d8b6142
Branimir Amidžić <mail@ambraspace.com>
2023-11-26 22:53:40 +0100

    Initial commit

---
+++ src/main/java/com/ambraspace/etprodaja/EtProdajaApplication.java
@@ -0,0 +1,13 @@
+package com.ambraspace.etprodaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtProdajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtProdajaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ambraspace/etprodaja/EtProdajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambraspace.etprodaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtProdajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
